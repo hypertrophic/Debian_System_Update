40ea9c7d752d81255ec23ca344add51e81b05a0a
Wissam Boubkir <kikijo.wb@gmail.com>
2023-08-22 23:16:58 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Check if the script is being run with superuser privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Please use sudo."
+   exit 1
+fi
+
+# Update package index
+apt-get update
+
+# Upgrade installed packages
+apt-get upgrade -y
+
+# Perform distribution upgrade
+apt-get dist-upgrade -y
+
+echo "Updates and upgrades complete."
